870d497548da7cc370bd79823a46c16fb4164774
if22b151 <if22b151@technikum-wien.at>
2023-04-19 10:56:10 +0200

    Error message works but not styled, output works but not centered

--- simpleJsonClient/controller.js
+++ simpleJsonClient/controller.js
@@ -1,13 +1,12 @@
 //Starting point for JQuery init
 $(document).ready(function () {
-
+    $("ol").hide();
+    $("#error").hide();
     loaddata();
 
 });
 
 function loaddata() {
-
-
     $.ajax({
         type: "GET",
         url: "../serviceHandler.php",
@@ -15,17 +14,36 @@ function loaddata() {
         data: {method: "getAppointments", param: "all"},
         dataType: "json",
         success: function (response) {
-
-            
+            //ToDo: Center container
+            $("#container").css({
+                "display" : "flex",
+                "justify-content" : "center",
+                "height" : "100%",
+            });
+            $("#ul").css({
+                "align-self" : "center"
+            });
 
             $.each (response, function (i, item) {
-
                 $('#list').append('<li>' + item["title"] + '</li>');
+                $('#list').append('<li>' + item["location"] + '</li>');
+                $('#ul').show();    
             });
+
+         },
+
+        error: function(e) {
+            $("#error").html("Error!").css({
+                "display": "grid",
+                "justify-content": "center",
+                "align-text": "center",
+                "color" : "red",
+                "font-size" : "x-large"
+            });
+            $("#error").show();
         }
 
     });
-
 
 }
 
